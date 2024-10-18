c5ca81617f417fd9e95b490f6d38c2d4bd5c878d
Bartosz Bujnicki <bbujnicki@internal.oculus-tech.com>
2024-10-18 15:05:53 +0200

    Add equals and hashcode for Team

--- src/main/java/scoreBoard/Team.java
+++ src/main/java/scoreBoard/Team.java
@@ -1,5 +1,7 @@
 package scoreBoard;
 
+import java.util.Objects;
+
 public class Team {
     private final String name;
 
@@ -9,5 +11,22 @@ public class Team {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Team team = (Team) o;
+        return Objects.equals(name, team.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }

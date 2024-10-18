22c0dda22c0684c8b730b555d8c6bd395191e8af
Bartosz Bujnicki <bbujnicki@internal.oculus-tech.com>
2024-10-18 12:03:13 +0200

    Init commit

---
+++ src/main/java/scoreBoard/Main.java
@@ -0,0 +1,9 @@
+package scoreBoard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+git remote add origin https://github.com/Bunzlau/scoreBoard.git

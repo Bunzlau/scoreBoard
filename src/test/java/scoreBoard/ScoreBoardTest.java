3300c819b624ae2bc70e011b36225e110d595270
Bartosz Bujnicki <bbujnicki@internal.oculus-tech.com>
2024-10-18 15:05:17 +0200

    Add test for checking the order in the ScoreBoard

--- src/test/java/scoreBoard/ScoreBoardTest.java
+++ src/test/java/scoreBoard/ScoreBoardTest.java
@@ -8,33 +8,69 @@ import org.junit.jupiter.api.Test;
 
 class ScoreBoardTest {
 
-    ScoreBoard scoreBoard;
+    private ScoreBoard scoreBoard;
+
+    private Team mexico;
+    private Team canada;
+    private Team brazil;
+    private Team spain;
+    private Team germany;
+    private Team france;
 
     @BeforeEach
-    void restartScoreBoard() {
+    void setUp() {
         scoreBoard = new ScoreBoard();
+        initializeTeams();
+    }
+
+    private void initializeTeams() {
+        mexico = new Team(Country.MEXICO.getCountryName());
+        canada = new Team(Country.CANADA.getCountryName());
+        brazil = new Team(Country.BRAZIL.getCountryName());
+        spain = new Team(Country.SPAIN.getCountryName());
+        germany = new Team(Country.GERMANY.getCountryName());
+        france = new Team(Country.FRANCE.getCountryName());
+    }
+
+    private void startAndSetScore(Team homeTeam, Team awayTeam, int homeScore, int awayScore) {
+        scoreBoard.startGame(homeTeam, awayTeam);
+        scoreBoard.updateGameScore(homeTeam, awayTeam, homeScore, awayScore);
     }
 
     @Test
     public void shouldStartTheGameWithInitialValue() {
-        final Match match = scoreBoard.startGame(new Team(Country.ARGENTINA.getCountryName()),
-                new Team(Country.AUSTRALIA.getCountryName()));
+        final Match match = scoreBoard.startGame(mexico, canada);
         final List<Match> matches = scoreBoard.getMatches();
 
-        assertTrue(matches.contains(match));
+        assertTrue(matches.contains(match), "The match should be added to the list of matches");
     }
 
     @Test
-    public void shouldUpdateTheScoreDuringTheMatch(){
-        final Team argentina = new Team(Country.ARGENTINA.getCountryName());
-        final Team australia = new Team(Country.AUSTRALIA.getCountryName());
-        scoreBoard.startGame(argentina,
-                australia);
-        final Match updatedMatch = scoreBoard.updateGameScore(argentina, australia, 1, 0);
+    public void shouldUpdateTheScoreDuringTheMatch() {
+        startAndSetScore(mexico, canada, 3, 2);
 
-        assertEquals(updatedMatch.getAwayTeamScore(), 0);
-        assertEquals(updatedMatch.getHomeTeamScore(), 1);
+        Match updatedMatch = scoreBoard.updateGameScore(mexico, canada, 1, 0);
+        assertEquals(1, updatedMatch.getHomeTeamScore(), "Home team score should be 1");
+        assertEquals(0, updatedMatch.getAwayTeamScore(), "Away team score should be 0");
+    }
 
+    @Test
+    public void shouldReturnSortedSummaryForTheGames() {
+        startAndSetScore(mexico, canada, 3, 2);
+        startAndSetScore(brazil, spain, 2, 2);
+        startAndSetScore(germany, france, 1, 1);
 
+        List<Match> summary = scoreBoard.getSummaryOfTheGames();
+
+        assertEquals(5, summary.get(0).getHomeTeamScore() + summary.get(0).getAwayTeamScore(), "Match with highest score should be first");
+        assertEquals(mexico, summary.get(0).getHomeWayTeam());
+
+        assertEquals(4, summary.get(1).getHomeTeamScore() + summary.get(1).getAwayTeamScore(), "Match with second highest score should be second");
+        assertEquals(brazil, summary.get(1).getHomeWayTeam());
+
+        assertEquals(2, summary.get(2).getHomeTeamScore() + summary.get(2).getAwayTeamScore(), "Match with lowest score should be last");
+        assertEquals(germany, summary.get(2).getHomeWayTeam());
+
+        assertEquals(3, summary.size(), "There should be 3 matches in total");
     }
 }
